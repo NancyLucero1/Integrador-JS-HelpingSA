657485f1573d71a381606dd8e27983965ebfe224
NancyLucero1 <138040991+NancyLucero1@users.noreply.github.com>
2023-09-25 14:31:19 -0300

    Segundo commit

--- assets/data.js
+++ assets/data.js
@@ -44,19 +44,27 @@ const productsData = [
   },
 
 ];
-
+//////////////////////////////  6
 const divideProductsInParts = (size) => {
+  // retornar la lista de productos
   let productsList = []
+  // 0 - 6 - 12
   for (let i = 0; i < productsData.length; i+= size) {
+    // [ [{},{},{},{},{},{}] , [{},{},{},{},{},{}], [{},{},{}]]
     productsList.push(productsData.slice(i,i + size))
 
   }
   return productsList;
 }
 
-const appState = {                            
-  products: divideProductsInParts(3), 
+// const [products, setState] = useState
+// const [currentProductsIndex, setState] = useState
+// const [productsLimit, setState] = useState
+// const [activeFilter, setState] = useState
+
+const appState = {                             //  0                     1              2
+  products: divideProductsInParts(2), // [ [{},{},{},{},{}] , [{},{},{},{},{}], [{},{},{},{},{}]] 3
   currentProductsIndex: 0,
-  productsLimit: divideProductsInParts(3).length, 
+  productsLimit: divideProductsInParts(2).length, // 3
   activeFilter: null
 }
